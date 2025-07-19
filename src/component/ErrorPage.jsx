f404598c0e4f6672b169c8faf494cb1e149c0dd1
jihad <zoyanopicial@gmail.com>
2025-07-19 22:46:52 +0600

    2d Error Handaling

--- src/component/ErrorPage.jsx
+++ src/component/ErrorPage.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
+import { useNavigate } from 'react-router'
 
 function ErrorPage() {
+    let HandelEffct = useNavigate();
+    const HandelError = () =>{
+        HandelEffct("/")
+    }
   return (
      <>
        <div className='text-center mt-14'>
         <h1 className='md:text-[150px] text-[50px] text-red-500 font-bold'>404</h1>
         <h1 className='text-3xl md:text-6xl text-red-900'>This Page is Not Avalible</h1>
-        <button>Please Back</button>
+        <button className='mt-7 bg-blue-800 px-10 py-3 rounded-xl text-white' onClick={HandelError}>Please Back</button>
        </div>
       
      </>
