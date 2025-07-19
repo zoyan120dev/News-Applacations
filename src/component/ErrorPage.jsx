9a6a374f24bd9f172f096254ae44641ad1a8633b
jihad <zoyanopicial@gmail.com>
2025-07-19 22:43:28 +0600

    Err page Design

--- src/component/ErrorPage.jsx
+++ src/component/ErrorPage.jsx
@@ -4,8 +4,8 @@ function ErrorPage() {
   return (
      <>
        <div className='text-center mt-14'>
-        <h1 className='md:text-[150px] text-[50px] text-red-500'>404</h1>
-        <h1>This Page is Not Avalible</h1>
+        <h1 className='md:text-[150px] text-[50px] text-red-500 font-bold'>404</h1>
+        <h1 className='text-3xl md:text-6xl text-red-900'>This Page is Not Avalible</h1>
         <button>Please Back</button>
        </div>
       
