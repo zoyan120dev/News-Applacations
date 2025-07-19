12d3ef81d325675bb20d3d6dea145d9450dbe168
jihad <zoyanopicial@gmail.com>
2025-07-19 21:47:21 +0600

    left section done

--- src/component/DashBord6.jsx
+++ src/component/DashBord6.jsx
@@ -3,9 +3,9 @@ function DashBord6() {
   return (
     <section className='bg-[#E4E4E780] p-6'>
       <div className='max-w-[1300px] mx-auto pt-0 pl-5 '>
-        <span className='text-gray-400 text-sm'>Home - Incidents</span>
+        <span className='text-gray-400 text-sm'>Incidents - DR-4699 March 2023 Severe Storms </span>
         <div className='flex md:justify-between justify-center flex-col lg:flex-row'>
-            <h1 className='text-[35px] font-bold text-center'>Incidents</h1>
+            <h1 className='text-[35px] font-bold text-center flex gap-2 items-center'> <div><img src="/fi_15330449 (2).png" alt="" /></div>DR-4699 March 2023 Severe Storms</h1>
 
             <div className='flex gap-5 items-center flex-col sm:flex-row'>
                <span className='bg-[#FAFAFA] p-3 rounded-xl 'shadow-md><i class="fa-solid fa-magnifying-glass"></i> <input   className='text-black'  placeholder='Search incident' type="search"/></span>
--- src/component/MainLocaions.jsx
+++ src/component/MainLocaions.jsx
@@ -5,8 +5,40 @@ function MainLocaions() {
   return (
    <>
     <DashBord6/>
-   <section className='max-w-[1300px] mx-auto mt-3'>
+   <section className='max-w-[1300px] mx-auto mt-9'>
+      {/* main sections div */}
+      <div>
+        {/* left section */}
+           <div>
 
+              <div className='flex items-center gap-2'>
+                <div className='bg-[#F4F4F5] p-3 rounded-full '>
+                    <img src="/map.png" alt="map-picture" />
+                </div>
+                <div>
+                    <span className='text-gray-500 text-sm'>Location</span>
+                    <h1 className='font-bold text-xl md:text-2xl'>Tulare County,  Los Angles, CA 23415</h1>
+                </div>
+              </div>
+
+                <div className='flex items-center gap-2 mt-4'>
+                <div className='bg-[#F4F4F5] p-3 rounded-full'>
+                    <img src="/icone.png" alt="map-picture" />
+                </div>
+                <div className='border-b border-gray-600 pb-5 w-full'>
+                    <span className='text-gray-500 text-sm'>Approx. Cost:</span>
+                    <h1 className='font-bold text-xl md:text-2xl'>$60,607,456.00</h1>
+                </div>
+              </div>
+                
+                <div className='border-b border-gray-600 pb-5 w-full my-4'>
+                    <h1 className='text-base md:text-2xl font-bold'>Description</h1>
+                     <p className='text-base md:text-lg text-gray-500 leading-tight'>Lorem ipsum dolar sit general sac mascho werhoLorem ipsum dolar sit general sac mascho werhoLorem ipsum dolar sit general sac mascho werhoLorem ipsum dolar sit general sac mascho werhoLorem ipsum dolar sit general sac mascho werhoLorem ipsum dolar sit general sac mascho werho</p>
+                </div>
+
+           </div>
+        {/* Right section */}
+      </div>
    </section>
    </>
   )
